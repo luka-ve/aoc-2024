50d46f8b1e64570d4be667b36bfa842046d42caa
= <=>
2024-12-01 11:16:14 +0100

    Clean up runner

--- aoc.py
+++ aoc.py
@@ -1,12 +1,20 @@
+import datetime
 from pathlib import Path
 import click
 import importlib
+import shutil
 from time import time
 
 
-@click.command()
-@click.argument("day")
-def aoc(day: str):
+@click.group(invoke_without_command=True)
+@click.pass_context
+def aoc(ctx):
+    pass
+
+
+@aoc.command()
+@click.argument("day", default=datetime.datetime.today().day, type=str)
+def run(day: str):
     module = importlib.import_module(f"{day}.solution")
 
     for part in (module.part_1, module.part_2):
@@ -14,10 +22,21 @@ def aoc(day: str):
             with open(Path.cwd() / day / inp) as f:
                 t0 = time()
                 result = part(f)
-                print(
+                click.echo(
                     f"{part.__name__} ran in {((time() - t0) * 1000):.5f} ms on {inp}"
                 )
-                print(result)
+                click.echo(result)
+
+
+@aoc.command()
+@click.argument("day", default=datetime.datetime.today().day, type=str)
+def create(day: str):
+    path = Path.cwd() / day
+    if path.exists():
+        click.echo(f'Directory "{path.as_posix()}" already exists')
+        return
+
+    shutil.copytree("template", day)
 
 
 if __name__ == "__main__":
